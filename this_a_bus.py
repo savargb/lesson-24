4b0a956925c6fce8159937ddacfa0e108f090471
savargb <savargb123@gmail.com>
2026-07-05 12:31:54 +0545

    hi

---
+++ this_a_bus.py
@@ -0,0 +1,11 @@
+class vehicles:
+    def __init__(self,name,maxspeed,mileage):
+        self.name = name
+        self.maxspeed = maxspeed
+        self.mileage = mileage
+
+class Bus(vehicles):
+    pass
+
+school_bus = Bus("Schllo Volve, 180, 12)")
+print("Vehicle Name:", school_bus.name, "Speed:", school_bus.maxspeed, "Mileage:")
