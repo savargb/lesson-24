d7b3e3641c83081c25b6914c75a5afbaa75ff242
savargb <savargb123@gmail.com>
2026-07-05 12:40:54 +0545

    hii

--- this_a_bus.py
+++ this_a_bus.py
@@ -8,4 +8,5 @@ class Bus(vehicles):
     pass
 
 school_bus = Bus("Schllo Volve, 180, 12)")
-print("Vehicle Name:", school_bus.name, "Speed:", school_bus.maxspeed, "Mileage:")
+print("Vehicle Name:", school_bus.name, "Speed:", school_bus.maxspeed, "Mileage:",
+school_bus.mileage)
